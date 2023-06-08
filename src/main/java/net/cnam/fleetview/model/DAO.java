528ff42dc2da366e1bac6c591f1659bb29b517c7
Alban <alban.rouault@hotmail.com>
2023-06-08 20:10:26 +0200

    Objet DAO

---
+++ src/main/java/net/cnam/fleetview/model/DAO.java
@@ -0,0 +1,68 @@
+package net.cnam.fleetview.model;
+
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * Classe abstraite générique DAO permettant de déclarer des objets d'accès à une base de données
+ *
+ * @param <T> Représente la classe des objets Java à manipuler
+ */
+public abstract class DAO<T> {
+
+    /**
+     * Attribut connection
+     */
+    protected Connection connection;
+
+    /**
+     * Constructeur de la classe DAO
+     *
+     * @param connection permet l'initialisation du champ connection
+     */
+    public DAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    /**
+     * Méthode de création d'un enregistrement
+     *
+     * @param obj un objet Auteur à écrire dans la base
+     * @return boolean qui vaut true si la création a réussi, false dans le cas
+     * contraire
+     */
+    public abstract boolean create(T obj);
+
+    /**
+     * Méthode de suppression d'un enregistrement
+     *
+     * @param obj un objet Auteur à supprimer dans la base
+     * @return boolean qui vaut true si la suppression a réussi, false dans le
+     * cas contraire
+     */
+    public abstract boolean delete(T obj);
+
+    /**
+     * Méthode de mise à jour d'un enregistrement
+     *
+     * @param obj un objet Auteur à mettre à jour dans la base
+     * @return boolean qui vaut true si la mise à jour a réussi, false dans le
+     * cas contraire
+     */
+    public abstract boolean update(T obj);
+
+    /**
+     * Retourne tous les objets de la table sous forme de liste
+     *
+     * @return
+     */
+    public abstract List<T> getAll();
+
+    /**
+     * Méthode de recherche des informations par l'id
+     *
+     * @param id l'identificateur à rechercher
+     * @return T
+     */
+    public abstract T selectById(int id);
+}
