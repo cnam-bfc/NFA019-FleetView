6544080cb7a3be8798e71365fb7cd9675ee510d1
L2oKcnam <leo.krys.auditeur@lecnam.net>
2023-06-09 18:09:32 +0200

    ParametrageBddView :  ajout de marge a l'entete(titre) et au bas de page (boutons).

--- src/main/java/net/cnam/fleetview/view/paramettre/ParametrageBddView.java
+++ src/main/java/net/cnam/fleetview/view/paramettre/ParametrageBddView.java
@@ -5,6 +5,14 @@ import net.cnam.fleetview.view.TitrePanel;
 import javax.swing.*;
 import java.awt.*;
 
+/*
+A faire :
+    - Agmenter taille titre
+    - Ajouter couleur au boutons
+    - arrondir les bords des champs de saisie
+
+ */
+
 public class ParametrageBddView extends JPanel {
 
     private final TitrePanel titrePanel;
@@ -12,15 +20,29 @@ public class ParametrageBddView extends JPanel {
     public ParametrageBddView() {
         super();
         this.setLayout(new BorderLayout());
+        //*********************************************
+        //--- Création des éléments de l'interface ---
+        //*********************************************
 
-        // Création des éléments de l'interface
+        //--- Titre ---
         this.titrePanel = new TitrePanel("\uF013", "BASE DE DONNEE");
+        JPanel jpEntete = new JPanel(new BorderLayout());
+
+        JPanel jpEnteteMargeLaterale = new JPanel();
+        jpEnteteMargeLaterale.setPreferredSize(new Dimension(20, 0));
+
+        JPanel jpEnteteMargeHorizontale = new JPanel();
+        jpEnteteMargeHorizontale.setPreferredSize(new Dimension(0, 20));
+
         JPanel jpPrincipale = new JPanel(new BorderLayout());
 
         JPanel jpBdInfo = new JPanel();
         jpBdInfo.setLayout(new BoxLayout(jpBdInfo, BoxLayout.PAGE_AXIS));
 
-        //ligne 1
+        //--- Contenu ---
+
+        //JPannel
+            //ligne 1
         JPanel jpLigneTop = new JPanel(new GridLayout(1,2));
         JPanel jpLigneTopIp = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JPanel jpLigneTopPort = new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -28,11 +50,11 @@ public class ParametrageBddView extends JPanel {
         jpIP.setLayout(new BoxLayout(jpIP, BoxLayout.PAGE_AXIS));
         JPanel jpPort = new JPanel(new BorderLayout());
         jpPort.setLayout(new BoxLayout(jpPort, BoxLayout.PAGE_AXIS));
-        //ligne 2
+            //ligne 2
         JPanel jpLigneMid = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JPanel jpNomDB = new JPanel(new BorderLayout());
         jpNomDB.setLayout(new BoxLayout(jpNomDB, BoxLayout.PAGE_AXIS));
-        //ligne 3
+            //ligne 3
         JPanel jpLigneBott = new JPanel(new GridLayout(1,2));
         JPanel jpLigneBottUtilisateur = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JPanel jpLigneBottPasse = new JPanel(new FlowLayout(FlowLayout.LEFT));
@@ -40,9 +62,8 @@ public class ParametrageBddView extends JPanel {
         jpNomUtilisateur.setLayout(new BoxLayout(jpNomUtilisateur, BoxLayout.PAGE_AXIS));
         JPanel jpMotDePasse = new JPanel(new BorderLayout());
         jpMotDePasse.setLayout(new BoxLayout(jpMotDePasse, BoxLayout.PAGE_AXIS));
-        JPanel jpActionHolder = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
-        //Marge
+            //Marge
         JPanel jpMargeGauche = new JPanel();
         jpMargeGauche.setPreferredSize(new Dimension(200, 0));
         JPanel jpMargeDroite = new JPanel();
@@ -55,28 +76,45 @@ public class ParametrageBddView extends JPanel {
         JLabel texteMotDePasse = new JLabel("Mot de passe");
 
         //champ texte
+            //ligne 1
         JTextField champIP = new JTextField();
         champIP.setPreferredSize( new Dimension( 200, 24 ) );
 
         JTextField champPort = new JTextField();
         champPort.setPreferredSize( new Dimension( 200, 24 ) );
-
+            //ligne 2
         JTextField champNomBD = new JTextField();
         champNomBD.setPreferredSize( new Dimension( 500, 24 ) );
-
+            //ligne 3
         JTextField champNomUtilisateur = new JTextField();
         champNomUtilisateur.setPreferredSize( new Dimension( 200, 24 ) );
 
         JTextField champMotDePasse = new JTextField();
         champMotDePasse.setPreferredSize( new Dimension( 200, 24 ) );
 
+        //--- BasDePage ---
+        //JPannel
+        JPanel jpActionHolder = new JPanel(new BorderLayout());
+        JPanel jpActionLine = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            //marge
+        JPanel jpMargeBasDePageMargeVerticale = new JPanel();
+        jpMargeBasDePageMargeVerticale.setPreferredSize(new Dimension(20, 0));
+        JPanel jpMargeBasDePageMargeHorizontale = new JPanel();
+        jpMargeBasDePageMargeHorizontale.setPreferredSize(new Dimension(0, 20));
+
+
+        //********************************
+        //--- Affectation des éléments ---
+        //********************************
+
+        //--- Entete ---
+        jpEntete.add(this.titrePanel, BorderLayout.CENTER);
+        jpEntete.add(jpEnteteMargeLaterale, BorderLayout.LINE_START);
+        jpEntete.add(jpEnteteMargeHorizontale, BorderLayout.NORTH);
+        jpPrincipale.add(jpEntete, BorderLayout.NORTH);
 
         //--- Contenu ---
-
-        // Ajout des éléments de l'interface
-        jpPrincipale.add(this.titrePanel, BorderLayout.NORTH);
-
-        //ligne 1
+            //ligne 1
         jpIP.add(texteIP);
         jpIP.add(champIP);
         jpLigneTopIp.add(jpIP);
@@ -87,13 +125,12 @@ public class ParametrageBddView extends JPanel {
         jpLigneTopPort.add(jpPort);
         jpLigneTop.add(jpLigneTopPort);
 
-        //ligne 2
+            //ligne 2
         jpNomDB.add(texteNomBD);
         jpNomDB.add(champNomBD);
         jpLigneMid.add(jpNomDB);
 
-
-        //ligne 3
+            //ligne 3
         jpNomUtilisateur.add(texteNomUtilisateur);
         jpNomUtilisateur.add(champNomUtilisateur);
         jpLigneBottUtilisateur.add(jpNomUtilisateur);
@@ -104,26 +141,31 @@ public class ParametrageBddView extends JPanel {
         jpLigneBottPasse.add(jpMotDePasse);
         jpLigneBott.add(jpLigneBottPasse);
 
-        // JPanel intermédiaire
+            // JPanel intermédiaire
         jpBdInfo.add(jpLigneTop);
         jpBdInfo.add(jpLigneMid);
         jpBdInfo.add(jpLigneBott);
 
         jpPrincipale.add(jpBdInfo, BorderLayout.CENTER);
-        jpPrincipale.setBackground(Color.BLACK);
-
-        // JPanel Validation/retour
-        JButton btnValider = new JButton("Valider");
-        JButton btnQuitter = new JButton("Quitter");
-
-        jpActionHolder.add(btnValider);
-        jpActionHolder.add(btnQuitter);
-
-        jpPrincipale.add(jpActionHolder, BorderLayout.SOUTH);
 
         //--- Marges ---
         jpPrincipale.add(jpMargeGauche, BorderLayout.LINE_START);
         jpPrincipale.add(jpMargeDroite, BorderLayout.LINE_END);
+
+        //--- Bloc Action ---
+
+        JButton btnValider = new JButton("Valider");
+        JButton btnQuitter = new JButton("Quitter");
+
+        jpActionLine.add(btnValider);
+        jpActionLine.add(btnQuitter);
+
+        jpActionHolder.add(jpActionLine, BorderLayout.CENTER);
+        jpActionHolder.add(jpMargeBasDePageMargeVerticale, BorderLayout.LINE_END);
+        jpActionHolder.add(jpMargeBasDePageMargeHorizontale, BorderLayout.SOUTH);
+        jpPrincipale.add(jpActionHolder, BorderLayout.SOUTH);
+
+
 
         this.add(jpPrincipale);
 
