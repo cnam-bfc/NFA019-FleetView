679e513e49c940920344a9f24e95a1803c5f52e0
Alban <alban.rouault@hotmail.com>
2023-06-17 21:09:14 +0200

    Ajout vue coursier

--- src/main/java/net/cnam/fleetview/view/CoursiersView.java
+++ src/main/java/net/cnam/fleetview/view/CoursiersView.java
@@ -1,6 +1,22 @@
 package net.cnam.fleetview.view;
 
+import net.cnam.fleetview.view.components.label.IconLabel;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class CoursiersView extends JPanel {
+    // ÉLÉMENTS DE L'INTERFACE
+    private final IconLabel iconLabel;
+
+    public CoursiersView() {
+        super();
+        this.setLayout(new BorderLayout());
+
+        // Création des éléments de l'interface
+        this.iconLabel = new IconLabel("\uF84A", "COURSIERS");
+
+        // Ajout des éléments de l'interface
+        this.add(this.iconLabel);
+    }
 }
