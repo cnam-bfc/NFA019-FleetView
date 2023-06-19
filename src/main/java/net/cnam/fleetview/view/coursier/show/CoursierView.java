b1e900a4d093a2e79cbfb32241aa760afcfe40d5
Alban <alban.rouault@hotmail.com>
2023-06-19 16:23:59 +0200

    mise à jour coursier view

--- src/main/java/net/cnam/fleetview/view/coursier/show/CoursierView.java
+++ src/main/java/net/cnam/fleetview/view/coursier/show/CoursierView.java
@@ -3,6 +3,8 @@ package net.cnam.fleetview.view.coursier.show;
 import net.cnam.fleetview.view.View;
 import net.cnam.fleetview.view.components.button.LabelButton;
 import net.cnam.fleetview.view.components.label.IconLabel;
+import net.cnam.fleetview.view.components.label.LabelInformation;
+import net.cnam.fleetview.view.components.row.RowLabelInformationForCoursierView;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -24,44 +26,14 @@ public class CoursierView extends View {
     private final JLabel matriculeLabel;
     private final JLabel matriculeContenu;
 
-    // Ligne 4 Sous titre
-    private final JPanel sousTitreUn;
-    private final JLabel sousTitreUnLabel;
+    // Ligne 4 contenu 1
+    private final RowLabelInformationForCoursierView contenuUnRow;
 
-    // Ligne 5 contenu
-    private final JPanel contenuUn;
-    private final JLabel contenuUnLabelUn;
-    private final JLabel contenuUnLabelDeux;
-    private final JLabel contenuUnLabelTrois;
-    private final JLabel contenuUnContenuUn;
-    private final JLabel contenuUnContenuDeux;
-    private final JLabel contenuUnContenuTrois;
+    // Ligne 5 contenu 2
+    private final RowLabelInformationForCoursierView contenuDeuxRow;
 
-    // Ligne 6 Sous titre
-    private final JPanel sousTitreDeux;
-    private final JLabel sousTitreDeuxLabel;
-
-    // Ligne 7 contenu
-    private final JPanel contenuDeux;
-    private final JLabel contenuDeuxLabelUn;
-    private final JLabel contenuDeuxLabelDeux;
-    private final JLabel contenuDeuxLabelTrois;
-    private final JLabel contenuDeuxContenuUn;
-    private final JLabel contenuDeuxContenuDeux;
-    private final JLabel contenuDeuxContenuTrois;
-
-    // Ligne 8 Sous titre
-    private final JPanel sousTitreTrois;
-    private final JLabel sousTitreTroisLabel;
-
-    // Ligne 9 contenu
-    private final JPanel contenuTrois;
-    private final JLabel contenuTroisLabelUn;
-    private final JLabel contenuTroisLabelDeux;
-    private final JLabel contenuTroisLabelTrois;
-    private final JLabel contenuTroisContenuUn;
-    private final JLabel contenuTroisContenuDeux;
-    private final JLabel contenuTroisContenuTrois;
+    // Ligne 6 contenu 3
+    private final RowLabelInformationForCoursierView contenuTroisRow;
 
     public CoursierView() {
         super();
@@ -75,7 +47,7 @@ public class CoursierView extends View {
 
         // Création des éléments de l'interface et ajout à l'interface
         // Ligne 1 titre
-        this.titre = new IconLabel("\uF84A", "COURSIER");
+        this.titre = new IconLabel("\uF84A", "");
         titre.getTexteLabel().setFont(new Font("Roboto", Font.BOLD, 32));
         this.add(this.titre);
 
@@ -98,93 +70,77 @@ public class CoursierView extends View {
         matriculeLayout.setHgap(10);
         this.matricule.setLayout(matriculeLayout);
         this.matriculeLabel = new JLabel("Matricule : ");
-        this.matriculeContenu = new JLabel("123456789");
+        this.matriculeContenu = new JLabel("");
         this.matricule.add(this.matriculeLabel);
         this.matricule.add(this.matriculeContenu);
         this.add(this.matricule);
 
-        // Ligne 4 Sous titre
-        this.sousTitreUn = new JPanel();
-        FlowLayout sousTitreUnLayout = new FlowLayout(FlowLayout.LEFT);
-        sousTitreUnLayout.setHgap(10);
-        this.sousTitreUn.setLayout(sousTitreUnLayout);
-        this.sousTitreUnLabel = new JLabel("Informations personnelles");
-        this.sousTitreUn.add(this.sousTitreUnLabel);
-        this.add(this.sousTitreUn);
+        // Ligne 4 contenu
+        JLabel contenuUnTitle = new JLabel("Courses");
+        LabelInformation contenuUnLabelUn = new LabelInformation("Semaine", "");
+        LabelInformation contenuUnLabelDeux = new LabelInformation("Mois" , "");
+        LabelInformation contenuUnLabelTrois = new LabelInformation("Année" , "");
+        this.contenuUnRow = new RowLabelInformationForCoursierView(contenuUnTitle, contenuUnLabelUn, contenuUnLabelDeux, contenuUnLabelTrois);
+        this.add(this.contenuUnRow);
 
         // Ligne 5 contenu
-        this.contenuUn = new JPanel();
-        GridLayout contenuUnLayout = new GridLayout(2, 3);
-        contenuUnLayout.setHgap(10);
-        this.contenuUn.setLayout(contenuUnLayout);
-        this.contenuUnLabelUn = new JLabel("Nom : ");
-        this.contenuUnLabelDeux = new JLabel("Prénom : ");
-        this.contenuUnLabelTrois = new JLabel("Date de naissance : ");
-        this.contenuUnContenuUn = new JLabel("Doe");
-        this.contenuUnContenuDeux = new JLabel("John");
-        this.contenuUnContenuTrois = new JLabel("01/01/1970");
-        this.contenuUn.add(this.contenuUnLabelUn);
-        this.contenuUn.add(this.contenuUnLabelDeux);
-        this.contenuUn.add(this.contenuUnLabelTrois);
-        this.contenuUn.add(this.contenuUnContenuUn);
-        this.contenuUn.add(this.contenuUnContenuDeux);
-        this.contenuUn.add(this.contenuUnContenuTrois);
-        this.add(this.contenuUn);
+        JLabel contenuDeuxTitle = new JLabel("Colis");
+        LabelInformation contenuDeuxLabelUn = new LabelInformation("Semaine", "");
+        LabelInformation contenuDeuxLabelDeux = new LabelInformation("Mois" , "");
+        LabelInformation contenuDeuxLabelTrois = new LabelInformation("Année" , "");
+        this.contenuDeuxRow = new RowLabelInformationForCoursierView(contenuDeuxTitle, contenuDeuxLabelUn, contenuDeuxLabelDeux, contenuDeuxLabelTrois);
+        this.add(this.contenuDeuxRow);
 
-        // Ligne 6 Sous titre
-        this.sousTitreDeux = new JPanel();
-        FlowLayout sousTitreDeuxLayout = new FlowLayout(FlowLayout.LEFT);
-        sousTitreDeuxLayout.setHgap(10);
-        this.sousTitreDeux.setLayout(sousTitreDeuxLayout);
-        this.sousTitreDeuxLabel = new JLabel("Informations professionnelles");
-        this.sousTitreDeux.add(this.sousTitreDeuxLabel);
-        this.add(this.sousTitreDeux);
+        // Ligne 6 contenu
+        JLabel contenuTroisTitle = new JLabel("Autre");
+        LabelInformation contenuTroisLabelUn = new LabelInformation("Accident(s)", "");
+        LabelInformation contenuTroisLabelDeux = new LabelInformation("Course en cours" , "");
+        LabelInformation contenuTroisLabelTrois = new LabelInformation("Cycle en cours" , "");
+        this.contenuTroisRow = new RowLabelInformationForCoursierView(contenuTroisTitle, contenuTroisLabelUn, contenuTroisLabelDeux, contenuTroisLabelTrois);
+        this.add(this.contenuTroisRow);
+    }
 
-        // Ligne 7 contenu
-        this.contenuDeux = new JPanel();
-        GridLayout contenuDeuxLayout = new GridLayout(3, 2);
-        contenuDeuxLayout.setHgap(10);
-        this.contenuDeux.setLayout(contenuDeuxLayout);
-        this.contenuDeuxLabelUn = new JLabel("Date d'embauche : ");
-        this.contenuDeuxLabelDeux = new JLabel("Nombre de livraisons : ");
-        this.contenuDeuxLabelTrois = new JLabel("Nombre d'accidents : ");
-        this.contenuDeuxContenuUn = new JLabel("01/01/1970");
-        this.contenuDeuxContenuDeux = new JLabel("123");
-        this.contenuDeuxContenuTrois = new JLabel("123");
-        this.contenuDeux.add(this.contenuDeuxLabelUn);
-        this.contenuDeux.add(this.contenuDeuxLabelDeux);
-        this.contenuDeux.add(this.contenuDeuxLabelTrois);
-        this.contenuDeux.add(this.contenuDeuxContenuUn);
-        this.contenuDeux.add(this.contenuDeuxContenuDeux);
-        this.contenuDeux.add(this.contenuDeuxContenuTrois);
-        this.add(this.contenuDeux);
+    public void setTitre (String titre) {
+        this.titre.getTexteLabel().setText(titre);
+    }
 
-        // Ligne 8 Sous titre
-        this.sousTitreTrois = new JPanel();
-        FlowLayout sousTitreTroisLayout = new FlowLayout(FlowLayout.LEFT);
-        sousTitreTroisLayout.setHgap(10);
-        this.sousTitreTrois.setLayout(sousTitreTroisLayout);
-        this.sousTitreTroisLabel = new JLabel("Informations de connexion");
-        this.sousTitreTrois.add(this.sousTitreTroisLabel);
-        this.add(this.sousTitreTrois);
+    public void setMatricule (String matricule) {
+        this.matriculeContenu.setText(matricule);
+    }
 
-        // Ligne 9 contenu
-        this.contenuTrois = new JPanel();
-        GridLayout contenuTroisLayout = new GridLayout(2, 2);
-        contenuTroisLayout.setHgap(10);
-        this.contenuTrois.setLayout(contenuTroisLayout);
-        this.contenuTroisLabelUn = new JLabel("Identifiant : ");
-        this.contenuTroisLabelDeux = new JLabel("Mot de passe : ");
-        this.contenuTroisLabelTrois = new JLabel("Rôle : ");
-        this.contenuTroisContenuUn = new JLabel("123456789");
-        this.contenuTroisContenuDeux = new JLabel("123456789");
-        this.contenuTroisContenuTrois = new JLabel("Coursier");
-        this.contenuTrois.add(this.contenuTroisLabelUn);
-        this.contenuTrois.add(this.contenuTroisLabelDeux);
-        this.contenuTrois.add(this.contenuTroisLabelTrois);
-        this.contenuTrois.add(this.contenuTroisContenuUn);
-        this.contenuTrois.add(this.contenuTroisContenuDeux);
-        this.contenuTrois.add(this.contenuTroisContenuTrois);
-        this.add(this.contenuTrois);
+    public void setCoursesSemaine(String coursesSemaine) {
+        this.contenuUnRow.getFirstLabel().getSecondLabel().setText(coursesSemaine);
+    }
+
+    public void setCoursesMois(String coursesMois) {
+        this.contenuUnRow.getSecondLabel().getSecondLabel().setText(coursesMois);
+    }
+
+    public void setCoursesAnnee(String coursesAnnee) {
+        this.contenuUnRow.getThirdLabel().getSecondLabel().setText(coursesAnnee);
+    }
+
+    public void setColisSemaine(String colisSemaine) {
+        this.contenuDeuxRow.getFirstLabel().getSecondLabel().setText(colisSemaine);
+    }
+
+    public void setColisMois(String colisMois) {
+        this.contenuDeuxRow.getSecondLabel().getSecondLabel().setText(colisMois);
+    }
+
+    public void setColisAnnee(String colisAnnee) {
+        this.contenuDeuxRow.getThirdLabel().getSecondLabel().setText(colisAnnee);
+    }
+
+    public void setAccidents(String accidents) {
+        this.contenuTroisRow.getFirstLabel().getSecondLabel().setText(accidents);
+    }
+
+    public void setCourseEnCours(String courseEnCours) {
+        this.contenuTroisRow.getSecondLabel().getSecondLabel().setText(courseEnCours);
+    }
+
+    public void setCycleEnCours(String cycleEnCours) {
+        this.contenuTroisRow.getThirdLabel().getSecondLabel().setText(cycleEnCours);
     }
 }
